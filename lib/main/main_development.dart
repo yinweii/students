b8f132d2f3696313f3317448d84ad07aadb1ddff
Dyan <thanhphong98ls@gmail.com>
2023-11-19 10:59:58 +0700

    [Feature] set up firebase dev for android

--- lib/main/main_development.dart
+++ lib/main/main_development.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:students/env/env_state.dart';
+import 'package:students/firebase_options.dart';
 import 'package:students/main/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   setupAndRunApp(env: EnvValue.development);
 }
